0480a0c707f96208efb7c48da9a72232d01ac44d
Abdul Hameed <ahameed@redhat.com>
2023-03-15 12:37:29 -0400

    generated api for inventory,connection and instance with below commands
    operator-sdk create api --group dbaas.redhat.com  --version v1alpha1 --kind ProviderInventory --resource --controller
    
    operator-sdk create api --group dbaas.redhat.com  --version v1alpha1 --kind ProviderConnection --resource --controller
    
    operator-sdk create api --group dbaas.redhat.com  --version v1alpha1 --kind ProviderInstance --resource --controller
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	dbaasredhatcomv1alpha1 "github.com/redhatHameed/provider-example-operator/api/v1alpha1"
+	"github.com/redhatHameed/provider-example-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(dbaasredhatcomv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,27 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ProviderInventoryReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ProviderInventory")
+		os.Exit(1)
+	}
+	if err = (&controllers.ProviderConnectionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ProviderConnection")
+		os.Exit(1)
+	}
+	if err = (&controllers.ProviderInstanceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ProviderInstance")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
